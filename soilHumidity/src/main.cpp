1d32f4848164db9c2d9d9aacfd62bd80f93cfe3c
Mark Sherstan <mshersta@ualberta.ca>
2019-01-23 11:14:27 -0700

    Use one sensor and one LED and vary blinking speeds.

--- soilHumidity/src/main.cpp
+++ soilHumidity/src/main.cpp
@@ -1,55 +1,45 @@
 #include <Arduino.h>
 
 // Declare Variables
-int ledPins[] = {11,12,13};
-int analogOut0, analogOut1, analogAvg;
-int idx;
+int ledPin = 13;
+int analogOut;
 
 
 
 void setup() {
   Serial.begin(9600);
 
-  // Set up LED's and set all to off
-  for (idx = 0; idx <= 7; idx++) {
-    pinMode(ledPins[idx],OUTPUT);
-    digitalWrite(ledPins[idx], LOW);
-  }
-
+  // Set up LED's and turn off
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 
 
 void loop() {
   // Read analog sensor and print results
-  analogOut0 = analogRead(A0);
-  analogOut1 = analogRead(A1);
-  analogAvg = (analogOut0 + analogOut1) / 2;
-  Serial.print(analogOut0); Serial.print("  ");
-  Serial.print(analogOut1); Serial.print("  ");
-  Serial.print(analogAvg);
+  analogOut = analogRead(A0);
+  Serial.print(analogOut);
 
   // Display LED based on humidity level
-  if (analogAvg < 200){
-    digitalWrite(ledPins[0], HIGH);
-    digitalWrite(ledPins[1], LOW);
-    digitalWrite(ledPins[2], LOW);
-
+  if (analogOut < 200){
     Serial.println("\tLow");
+    delay(500);
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
 
-  } else if (analogAvg >= 200 && analogAvg < 400){
-    digitalWrite(ledPins[0], LOW);
-    digitalWrite(ledPins[1], HIGH);
-    digitalWrite(ledPins[2], LOW);
-
+  } else if (analogOut >= 200 && analogOut < 400){
     Serial.println("\tMid");
+    digitalWrite(ledPin, HIGH);
 
   } else {
-    digitalWrite(ledPins[0], HIGH);
-    digitalWrite(ledPins[1], LOW);
-    digitalWrite(ledPins[2], HIGH);
-
     Serial.println("\tHigh");
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
   }
+
 
 }
