868e27877e3ae53b6ba6d73e03e3e111c9b80982
Mark Sherstan <mshersta@ualberta.ca>
2019-03-03 18:54:00 -0700

    Created function for multiple entries

--- mintAutomation/mintCSVImport.py
+++ mintAutomation/mintCSVImport.py
@@ -1,48 +1,55 @@
 import pyautogui
 import pandas as pd
 import time
+import datetime
+
+# Get current time stamp
+now = datetime.datetime.now()
+timeStamp = now.strftime("%Y-%m-%d")
 
 # Set up auto GUI
-#pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 0.1
 
 # Button locations
 newTransaction = (540, 430)
 date = (425, 460)
-description = (545, 460)
-category = (780, 460)
-amount = (910, 460)
 notes = (500, 660)
 done = (900, 760)
-checkBox = (470, 570)
 
 # Read in CSV data
 data = pd.read_csv('data.csv', header = 0)
-print(data)
 
+# Give time for user to navigate to page
 print("Load webpage at full size. You have 3 seconds")
 for ii in range(3):
     print('.', end='', flush=True)
     time.sleep(1)
 
-for ii in range(1):
-      pyautogui.click(newTransaction, duration=0.25)
+# Function for entering data
+def createEntry(idx):
+     pyautogui.click(newTransaction)
 
-      pyautogui.click(date)
-      pyautogui.hotkey('command', 'a')
-      pyautogui.press('delete')
-      pyautogui.typewrite(data.iat[0,0])
-      pyautogui.press('tab')
+     pyautogui.click(date)
+     pyautogui.hotkey('command', 'a')
+     pyautogui.press('delete')
+     pyautogui.typewrite(data.iat[idx,0])
+     pyautogui.press('tab')
 
-      pyautogui.typewrite(data.iat[0,1])
-      pyautogui.press('tab')
+     pyautogui.typewrite(data.iat[idx,1])
+     pyautogui.press('tab')
 
-      pyautogui.typewrite(data.iat[0,2])
-      pyautogui.press('tab')
+     pyautogui.typewrite(data.iat[idx,2])
+     pyautogui.press('tab')
 
-      pyautogui.typewrite(str(data.iat[0,3]))
+     pyautogui.typewrite(str(data.iat[idx,3]))
 
-      pyautogui.click(notes)
-      pyautogui.typewrite("Imported on ?? from script")
+     pyautogui.click(notes)
+     pyautogui.typewrite("Imported on "+ timeStamp)
 
-      pyautogui.click(done)
+     pyautogui.click(done)
+
+# Loop through all the csv data and enter with the createEntry function
+for ii in range(len(data)):
+    time.sleep(1)
+    createEntry(ii)
