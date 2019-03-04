88abee83d34a03d78e43bf131f954a74a63f1933
Mark Sherstan <mshersta@ualberta.ca>
2019-03-03 18:29:26 -0700

    Changed clicks to tabs for speed increase

--- mintAutomation/mintCSVImport.py
+++ mintAutomation/mintCSVImport.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 
 # Set up auto GUI
-pyautogui.PAUSE = 1
+#pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
 # Button locations
@@ -12,37 +12,37 @@ date = (425, 460)
 description = (545, 460)
 category = (780, 460)
 amount = (910, 460)
-notes = (500, 640)
-done = (900, 720)
+notes = (500, 660)
+done = (900, 760)
+checkBox = (470, 570)
 
 # Read in CSV data
 data = pd.read_csv('data.csv', header = 0)
 print(data)
 
-print("Load webpage at full size. You have 10 seconds")
-
-for ii in range(5):
+print("Load webpage at full size. You have 3 seconds")
+for ii in range(3):
     print('.', end='', flush=True)
-    time.sleep(2)
-#
+    time.sleep(1)
+
 for ii in range(1):
       pyautogui.click(newTransaction, duration=0.25)
 
-      pyautogui.click(date, duration=0.25)
+      pyautogui.click(date)
       pyautogui.hotkey('command', 'a')
       pyautogui.press('delete')
       pyautogui.typewrite(data.iat[0,0])
+      pyautogui.press('tab')
 
-      pyautogui.click(description, duration=0.25)
       pyautogui.typewrite(data.iat[0,1])
+      pyautogui.press('tab')
 
-      pyautogui.click(category, duration=0.25)
       pyautogui.typewrite(data.iat[0,2])
+      pyautogui.press('tab')
 
-      pyautogui.click(amount, duration=0.25)
       pyautogui.typewrite(str(data.iat[0,3]))
 
-      pyautogui.click(notes, duration=0.25)
+      pyautogui.click(notes)
       pyautogui.typewrite("Imported on ?? from script")
 
-      pyautogui.click(done, duration=0.25)
+      pyautogui.click(done)
