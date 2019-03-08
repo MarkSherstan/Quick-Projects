9072b44318182cd327e5634f049df2218b985780
Mark Sherstan <mshersta@ualberta.ca>
2019-03-08 11:33:27 -0700

    Initial commit real time plotter .m function.

---
+++ matlabFunctions/realTimePlotter.m
@@ -0,0 +1,32 @@
+function [] = realTimePlotter()
+  % Set up figure, get properties, and label
+  figure
+  h = animatedline;
+  ax = gca;
+  ax.YLim = [0 5];
+  xlabel('Time (s)')
+  ylabel('Output Y [units]')
+
+  % Start a timer
+  tic
+  startTime = toc;
+
+  % Loop for 20 secounds
+  while toc < 20
+    % Random data
+    v = randi(4);
+
+    % Get current time and add to animation
+    t = toc - startTime;
+    addpoints(h,t,v)
+
+    % Update axes
+    if t < 5
+        ax.XLim = [0 10];
+        drawnow
+    else
+        ax.XLim = [t-5 t+5];
+        drawnow
+    end
+
+  end
