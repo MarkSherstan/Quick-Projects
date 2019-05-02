671145866a682518f62fb1225b279445fff82b90
Mark Sherstan <mshersta@ualberta.ca>
2019-05-02 10:26:38 -0600

    Initial commit of ultrasonic sensor

---
+++ 37in1sensorKit/ultrasonicSensor/ultrasonicSensor.ino
@@ -0,0 +1,37 @@
+// Define variables
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+
+
+void setup() {
+  // Setup serial port
+  Serial.begin(9600);
+
+  // Setup digital pins input and output
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+
+void loop() {
+  // Clear the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(3);
+
+  // Set the trigPin on HIGH for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // distance = traveltime x speed of sound [cm/us] x 1/2
+  distance = duration * 0.0343 * 0.5;
+
+  // Print
+  Serial.print(distance);
+  Serial.println(" cm");
+}
