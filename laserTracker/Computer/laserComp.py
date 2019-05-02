2ddf1e8169a81112295c5484dff618869a891c99
Mark Sherstan <mshersta@ualberta.ca>
2019-05-01 23:04:19 -0600

    Update background detection algorithm and laser tracking.

--- laserTracker/Computer/laserComp.py
+++ laserTracker/Computer/laserComp.py
@@ -10,13 +10,16 @@ class video:
 		# Video properties
 		self.width = 720
 		self.height = 480
-		self.FPS = 30
+		self.FPS = 10
 
 		# HSV mask range
-		self.HSVlower = np.array([0,0,0])
-		self.HSVupper = np.array([180,255,255])
+		self.HSVlower = np.array([140,0,200])
+		self.HSVupper = np.array([160,255,255])
 
-		# Define location of object and laser
+		# Kernal
+		self.kernel = np.ones((7,7), np.uint8)
+
+		# Location of object and laser
 		self.objectX = 0
 		self.objectY = 0
 		self.laserX = 0
@@ -24,65 +27,62 @@ class video:
 
 	def startCamera(self):
 		# Create video capture object and configure settings
-		self.cam = cv2.VideoCapture(0)
+		self.cam = cv2.VideoCapture(1)
 		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
 		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 		self.cam.set(cv2.CAP_PROP_FPS, self.FPS)
 
-		# Get rid of initial garbo frames
-		for ii in range(10):
-			_, frame = self.cam.read()
-
-		# Get a good first frame for mask
-		_, frame = self.cam.read()
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
-		self.firstFrame = gray
+		# Build the foreground/background detection algorithm
+		self.subtractor = cv2.createBackgroundSubtractorMOG2(history=600, varThreshold=100, detectShadows=False)
 
 	def findObject(self):
 		# Read a frame
 		_, frame = self.cam.read()
 
-		# Removes high frequency components and converts to HSV color space
-		frameBlur = cv2.GaussianBlur(frame, (7, 7), 0)
-		HSV = cv2.cvtColor(frameBlur, cv2.COLOR_BGR2HSV)
-		gray = cv2.cvtColor(frameBlur, cv2.COLOR_BGR2GRAY)
+		# Dilate the frame and convert to HSV
+		frameDilate = cv2.dilate(frame, self.kernel, iterations=1)
+		HSV = cv2.cvtColor(frameDilate, cv2.COLOR_BGR2HSV)
 
-		# Mask for the laser dot
-		mask = cv2.inRange(HSV, self.HSVlower, self.HSVupper)
-		laser = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		# Laser mask
+		laserMask = cv2.inRange(HSV, self.HSVlower, self.HSVupper)
+
+		# cv2.RETR_EXTERNAL: Only extreme outer flags. All child contours are left behind.
+		# CHAIN_APPROX_SIMPLE: Removes all redundant points and compresses the contour.
+		laser = cv2.findContours(laserMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		laser = imutils.grab_contours(laser)
 
-		# only proceed if laser was found
+		# Only proceed if laser was found
 		if len(laser) > 0:
 			c = max(laser, key=cv2.contourArea)
 			((x, y), radius) = cv2.minEnclosingCircle(c)
+			cv2.circle(frame, (int(x), int(y)), int(radius), (0,0,255), 1)
 			self.laserX = int(x)
 			self.laserY = int(y)
 
-		# compute the absolute difference between the current frame and first frame
-		frameDelta = cv2.absdiff(self.firstFrame, gray)
-		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-		# dilate the thresholded image to fill in holes, then find contours on thresholded image
+		# Motion mask
+		motionMask = self.subtractor.apply(frame)
+		motionMask = cv2.erode(motionMask, self.kernel, iterations=1)
+		motionMask = cv2.dilate(motionMask, self.kernel, iterations=1)
+
 		# cv2.RETR_EXTERNAL: Only extreme outer flags. All child contours are left behind.
 		# CHAIN_APPROX_SIMPLE: Removes all redundant points and compresses the contour.
-		thresh = cv2.dilate(thresh, None, iterations=2)
-		motion = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		motion = cv2.findContours(motionMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		motion = imutils.grab_contours(motion)
 
 		# only proceed if at least one contour was found
 		if len(motion) > 0:
 			c = max(motion, key=cv2.contourArea)
 			((x, y), radius) = cv2.minEnclosingCircle(c)
+			cv2.circle(frame, (int(x), int(y)), int(radius), (255,0,0), 1)
 			self.objectX = int(x)
 			self.objectY = int(y)
 
 		# Show the frames
 		key = cv2.waitKey(1) & 0xFF
 		cv2.imshow('Frame', frame)
-		cv2.imshow('Laser', mask)
-		cv2.imshow('Motion', thresh)
+		# cv2.imshow('Laser', laserMask)
+		# cv2.imshow('Motion', motionMask)
 
 	def stopCamera(self):
 		# Clear connections
@@ -94,6 +94,7 @@ def main():
 	v = video()
 	v.startCamera()
 
+	# Run for a set amount of time
 	T = int(input("Enter time to run program: "))
 	startTime = time.time()
 	count = 0
@@ -103,9 +104,10 @@ def main():
 		print(count, v.laserX, v.laserY, v.objectX, v.objectY)
 		count += 1
 
+	# Print average frame rate
 	print("Frames per second: ", int(count / (time.time()-startTime)))
 
-	# Close all connections on the pi
+	# Close all connections
 	v.stopCamera()
 
 
