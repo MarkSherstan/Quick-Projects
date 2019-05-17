c341c59c4fbd77f2f1e090290d4f30c24bb84a90
Mark Sherstan <mshersta@ualberta.ca>
2019-05-16 23:31:47 -0600

    Formatting update.

--- OpenCV/Python/HSVslider.py
+++ OpenCV/Python/HSVslider.py
@@ -10,7 +10,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Dummy function
 def nothing(x):
-    pass
+	pass
 
 # Creating a window for later use
 cv2.namedWindow('result')
@@ -33,35 +33,35 @@ cv2.setTrackbarPos('s_u', 'result', 255)
 cv2.setTrackbarPos('v_u', 'result', 255)
 
 while(True):
-    # Get frame
-    _, frame = cap.read()
+	# Get frame
+	_, frame = cap.read()
 
-    # Convert to HSV
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+	# Convert to HSV
+	hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-    # Get info from tracking bar(s)
-    h_l = cv2.getTrackbarPos('h_l', 'result')
-    s_l = cv2.getTrackbarPos('s_l', 'result')
-    v_l = cv2.getTrackbarPos('v_l', 'result')
+	# Get info from tracking bar(s)
+	h_l = cv2.getTrackbarPos('h_l', 'result')
+	s_l = cv2.getTrackbarPos('s_l', 'result')
+	v_l = cv2.getTrackbarPos('v_l', 'result')
 
-    h_u = cv2.getTrackbarPos('h_u', 'result')
-    s_u = cv2.getTrackbarPos('s_u', 'result')
-    v_u = cv2.getTrackbarPos('v_u', 'result')
+	h_u = cv2.getTrackbarPos('h_u', 'result')
+	s_u = cv2.getTrackbarPos('s_u', 'result')
+	v_u = cv2.getTrackbarPos('v_u', 'result')
 
-    # Masking and result
-    lower = np.array([h_l,s_l,v_l])
-    upper = np.array([h_u,s_u,v_u])
-    mask = cv2.inRange(hsv, lower, upper)
+	# Masking and result
+	lower = np.array([h_l,s_l,v_l])
+	upper = np.array([h_u,s_u,v_u])
+	mask = cv2.inRange(hsv, lower, upper)
 
-    result = cv2.bitwise_and(frame,frame,mask = mask)
+	result = cv2.bitwise_and(frame,frame,mask = mask)
 
-    # Show result
-    cv2.imshow('result', result)
+	# Show result
+	cv2.imshow('result', result)
 
-    # Exit commands
-    key = cv2.waitKey(5) & 0xFF
-    if key == ord("q"):
-        break
+	# Exit commands
+	key = cv2.waitKey(5) & 0xFF
+	if key == ord("q"):
+		break
 
 # Release cam and close windows
 cap.release()
