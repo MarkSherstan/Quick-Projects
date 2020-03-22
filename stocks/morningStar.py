9657c7ee37c3d1f9abce97bfe48556f006d1f876
Mark Sherstan <mshersta@ualberta.ca>
2020-03-22 16:52:51 -0600

    added USD and CAD error checking

--- stocks/morningStar.py
+++ stocks/morningStar.py
@@ -1,5 +1,5 @@
 # Download data from https://www.morningstar.ca/ca/
-# Morning star (select stock) -> performance -> Key ratios -> full key ratios data -> export to .csv
+# Morning star (select stock) -> performance -> key ratios -> full key ratios data -> export to .csv
 
 # Required packages
 import pandas as pd
@@ -13,30 +13,46 @@ USD2CAD = 1/CAD2USD
 # Master dataframe for all the data
 masterDF = pd.DataFrame()
 
+# Loop through the data directory looking only at .csv files
 for file in glob.iglob('data/*.csv'):
     # Set the stock name
     stockName = file.replace('data/','').replace(' Key Ratios.csv','')
 
-    # Read file in data folder and manipulate the dataframe for consitency
+    # Read current file in data folder and manipulate the dataframe for consistency
     df = pd.read_csv(file, skiprows=[0,1], sep=",", header=0, index_col=0)
     df = df.drop(labels='TTM', axis=1)
     df.columns = range(len(df.columns))
     
-    # Pull the data to be analyzed
-    netIncome = df.loc['Net Income USD Mil'].str.replace(',', '').astype(float)
-    dividends = df.loc['Dividends USD',:].str.replace(',', '').astype(float)
-    shares = df.loc['Shares Mil',:].str.replace(',', '').astype(float)
-    payoutRatio = df.loc['Payout Ratio % *',:].str.replace(',', '').astype(float)
-    returnOnEquity = df.loc['Return on Equity %',:].str.replace(',', '').astype(float)
-    revenue = df.loc['Revenue USD Mil',:].str.replace(',', '').astype(float)
-    netMargin = df.loc['Net Margin %',:].str.replace(',', '').astype(float)
-    opCashFlow = df.loc['Operating Cash Flow USD Mil',:].str.replace(',', '').astype(float)
-    longTermDebt = df.loc['Long-Term Debt',:].str.replace(',', '').astype(float)
-    earningsPerShare = df.loc['Earnings Per Share USD',:].str.replace(',', '').astype(float)
-    currentAssets = df.loc['Total Current Assets',:].str.replace(',', '').astype(float)
-    currentLiabilities = df.loc['Total Current Liabilities',:].str.replace(',', '').astype(float)
-    bookValuePerShare = df.loc['Book Value Per Share * USD',:].str.replace(',', '').astype(float)
-    
+    # Pull the data to be analyzed (always in USD)
+    try:
+        netIncome = df.loc['Net Income USD Mil'].str.replace(',', '').astype(float)
+        dividends = df.loc['Dividends USD',:].str.replace(',', '').astype(float)
+        shares = df.loc['Shares Mil',:].str.replace(',', '').astype(float)
+        payoutRatio = df.loc['Payout Ratio % *',:].str.replace(',', '').astype(float)
+        returnOnEquity = df.loc['Return on Equity %',:].str.replace(',', '').astype(float)
+        revenue = df.loc['Revenue USD Mil',:].str.replace(',', '').astype(float)
+        netMargin = df.loc['Net Margin %',:].str.replace(',', '').astype(float)
+        opCashFlow = df.loc['Operating Cash Flow USD Mil',:].str.replace(',', '').astype(float)
+        longTermDebt = df.loc['Long-Term Debt',:].str.replace(',', '').astype(float)
+        earningsPerShare = df.loc['Earnings Per Share USD',:].str.replace(',', '').astype(float)
+        currentAssets = df.loc['Total Current Assets',:].str.replace(',', '').astype(float)
+        currentLiabilities = df.loc['Total Current Liabilities',:].str.replace(',', '').astype(float)
+        bookValuePerShare = df.loc['Book Value Per Share * USD',:].str.replace(',', '').astype(float)
+    except:
+        netIncome = df.loc['Net Income CAD Mil'].str.replace(',', '').astype(float) * CAD2USD
+        dividends = df.loc['Dividends CAD',:].str.replace(',', '').astype(float) * CAD2USD
+        shares = df.loc['Shares Mil',:].str.replace(',', '').astype(float)
+        payoutRatio = df.loc['Payout Ratio % *',:].str.replace(',', '').astype(float)
+        returnOnEquity = df.loc['Return on Equity %',:].str.replace(',', '').astype(float)
+        revenue = df.loc['Revenue CAD Mil',:].str.replace(',', '').astype(float) * CAD2USD
+        netMargin = df.loc['Net Margin %',:].str.replace(',', '').astype(float)
+        opCashFlow = df.loc['Operating Cash Flow CAD Mil',:].str.replace(',', '').astype(float) * CAD2USD
+        longTermDebt = df.loc['Long-Term Debt',:].str.replace(',', '').astype(float) * CAD2USD
+        earningsPerShare = df.loc['Earnings Per Share CAD',:].str.replace(',', '').astype(float) * CAD2USD
+        currentAssets = df.loc['Total Current Assets',:].str.replace(',', '').astype(float) * CAD2USD
+        currentLiabilities = df.loc['Total Current Liabilities',:].str.replace(',', '').astype(float) * CAD2USD
+        bookValuePerShare = df.loc['Book Value Per Share * CAD',:].str.replace(',', '').astype(float) * CAD2USD
+
     # Calculate a few ratios
     revenue_share = revenue/shares
     opCashFlow_share = opCashFlow/shares
@@ -44,21 +60,21 @@ for file in glob.iglob('data/*.csv'):
     currentRatio = currentAssets/currentLiabilities
     
     # Only log data that is important
-    netIncome['ID'] = stockName + '_netIncome'
-    dividends['ID'] = stockName + '_dividends'
-    shares['ID'] = stockName + '_shares'
-    payoutRatio['ID'] = stockName + '_payoutRatio'
-    returnOnEquity['ID'] = stockName + '_returnOnEquity'
-    revenue['ID'] = stockName + '_revenue'
-    opCashFlow['ID'] = stockName + '_opCashFlow'
-    longTermDebt['ID'] = stockName + '_longTermDebt'
-    netMargin['ID'] = stockName + '_netMargin'
-    revenue_share['ID'] = stockName + '_revenue/share'
-    opCashFlow_share['ID'] = stockName + '_opCashFlow/share'
-    longTermDebt_share['ID'] = stockName + '_longTermDebt/share'
-    earningsPerShare['ID'] = stockName + '_earningsPerShare'
-    bookValuePerShare['ID'] = stockName + '_bookValuePerShare'
-    currentRatio['ID'] = stockName + '_currentRatio'
+    netIncome['ID'] = 'netIncome' + stockName
+    dividends['ID'] = 'dividends' + stockName
+    shares['ID'] = 'shares' + stockName
+    payoutRatio['ID'] = 'payoutRatio' + stockName
+    returnOnEquity['ID'] = 'returnOnEquity' + stockName
+    revenue['ID'] = 'revenue' + stockName
+    opCashFlow['ID'] = 'opCashFlow' + stockName
+    longTermDebt['ID'] = 'longTermDebt' + stockName
+    netMargin['ID'] = 'netMargin' + stockName
+    revenue_share['ID'] = 'revenue/share' + stockName
+    opCashFlow_share['ID'] = 'opCashFlow/share' + stockName
+    longTermDebt_share['ID'] = 'longTermDebt/share' + stockName
+    earningsPerShare['ID'] = 'earningsPerShare' + stockName
+    bookValuePerShare['ID'] = 'bookValuePerShare' + stockName
+    currentRatio['ID'] = 'currentRatio' + stockName
     
     # Combine this stocks data into a single df and merge with the rest of the data
     objs = [netIncome, dividends, shares, payoutRatio, returnOnEquity, revenue, opCashFlow, longTermDebt, netMargin,
@@ -69,4 +85,3 @@ for file in glob.iglob('data/*.csv'):
     
 # Save the data to a csv
 masterDF.to_csv('stocksMerged.csv')
-
