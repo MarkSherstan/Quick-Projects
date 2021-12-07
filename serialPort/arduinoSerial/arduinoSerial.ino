6aabbd27e00c7ad0989859587bbc002db8b2e43c
Mark Sherstan <mshersta@ualberta.ca>
2021-12-06 22:57:21 -0700

    Tidy formatting and var names

--- serialPort/arduinoSerial/arduinoSerial.ino
+++ serialPort/arduinoSerial/arduinoSerial.ino
@@ -1,28 +1,26 @@
-int analogValue;
-
-
-void setup() {
-  // Setup serial port
-  Serial.begin(9600);
+void setup()
+{
+    // Setup serial port
+    Serial.begin(9600);
 }
 
+void loop()
+{
+    // Read the analog pin
+    int analogValue = analogRead(A0);
 
-void loop() {
-  // Read the analog pin
-  analogValue = analogRead(A0);
-
-  // Write bytes via serial
-  writeBytes(&analogValue);
+    // Write bytes via serial
+    writeBytes(&analogValue);
 }
 
+void writeBytes(int* data)
+{
+    // Cast to a byte pointer
+    byte* byteData = (byte*)(data);
 
-void writeBytes(int* data1){
-  // Cast to a byte pointer
-  byte* byteData1 = (byte*)(data1);
+    // Byte array with header for transmission
+    byte buf[4] = {0x9F, 0x6E, byteData[0], byteData[1]};
 
-  // Byte array with header for transmission
-  byte buf[4] = {0x9F, 0x6E, byteData1[0], byteData1[1]};
-
-  // Write the byte
-  Serial.write(buf, 4);
+    // Write the byte
+    Serial.write(buf, 4);
 }
